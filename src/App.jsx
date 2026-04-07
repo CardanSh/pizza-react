3053f10a08b249ed87393edd26df44146fad25ca
Deev Maksim <deev.maks2012@ya.ru>
2026-04-07 21:17:05 +0500

    catalog create

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-
+import Catalog from "./components/Catalog"
+import Header from "./components/Header"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [productList, setProductList] = useState([]);
+  useEffect(loadProducts, []);
+
+  function loadProducts() {
+    fetch('http://10.0.20.34:3000/api/v1/product')
+      .then((response) => {
+        return response.json();
+      })
+      .then((products) => {
+        setProductList(products);
+      })
+  }
+
   return (
-    <></>
+    <>
+      <Header />
+      <Catalog list={productList} />
+    </>
   )
 }
 
